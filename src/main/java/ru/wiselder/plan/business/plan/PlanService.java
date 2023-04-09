d45057f75972135e2c443d58422e6bdae707eb50
wiselder <wiselder@ya.ru>
2023-04-09 07:48:00 +0300

    set NOT_FOUND status on non existing groups or teachers

--- src/main/java/ru/wiselder/plan/business/plan/PlanService.java
+++ src/main/java/ru/wiselder/plan/business/plan/PlanService.java
@@ -3,10 +3,15 @@ package ru.wiselder.plan.business.plan;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import ru.wiselder.plan.business.faculty.FacultyDao;
+import ru.wiselder.plan.business.teacher.TeacherDao;
 import ru.wiselder.plan.model.Bell;
 import ru.wiselder.plan.model.Lesson;
+import ru.wiselder.plan.request.GroupRequest;
 import ru.wiselder.plan.response.GroupLesson;
 import ru.wiselder.plan.request.GroupDayPlanRequest;
 import ru.wiselder.plan.request.GroupWeekPlanRequest;
@@ -18,29 +23,45 @@ import ru.wiselder.plan.response.WeekPlan;
 @Service
 @RequiredArgsConstructor
 public class PlanService {
+    private final FacultyDao facultyDao;
+    private final TeacherDao teacherDao;
     private final PlanDao planDao;
     private final PlanFormer planFormer;
 
     @Transactional
     public DayPlan getPlan(GroupDayPlanRequest request) {
+        var group = request.group();
+        facultyDao.findGroup(group.faculty(), new GroupRequest(group.course(), group.number(), group.subNumber()))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
         var lessons = toGroupLessons(planDao.findLessons(request));
         return planFormer.form(request.day(), getAllBells(), lessons, request.group());
     }
 
+
     @Transactional
-    public WeekPlan getPlan(GroupWeekPlanRequest request) {
-        var lessons = toGroupLessons(planDao.findLessons(request));
-        return planFormer.form(getAllBells(), lessons, request);
+    public WeekPlan getPlan(GroupWeekPlanRequest group) {
+        facultyDao.findGroup(group.faculty(), new GroupRequest(group.course(), group.number(), group.subNumber()))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var lessons = toGroupLessons(planDao.findLessons(group));
+        return planFormer.form(getAllBells(), lessons, group);
     }
 
     @Transactional
     public DayPlan getPlan(TeacherDayPlanRequest request) {
+        teacherDao.findById(request.teacherId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
         var lessons = toGroupLessons(planDao.findLessons(request));
         return planFormer.form(request.day(), getAllBells(), lessons, request.teacherId());
     }
 
     @Transactional
     public WeekPlan getPlan(TeacherWeekPlanRequest request) {
+        teacherDao.findById(request.teacherId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
         var lessons = toGroupLessons(planDao.findLessons(request));
         return planFormer.form(getAllBells(), lessons, request.teacherId());
     }
